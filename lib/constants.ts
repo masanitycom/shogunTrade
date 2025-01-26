6dde74a29f048e02d7df8baca2ff7d16e2ad444c
masanitycom <masataka.tak@gmail.com>
2025-01-26 13:18:42 +0900

    Initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const FEE_RATES = {
+  evo: 0.01, // 1% fee for EVO Card
+  other: 0.03, // 3% fee for other wallets
+}
+
+export const PROFIT_DISTRIBUTION_PERCENTAGE = 20 // Default is 20%
+
